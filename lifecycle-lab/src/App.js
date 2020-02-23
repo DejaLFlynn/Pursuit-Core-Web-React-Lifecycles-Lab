ddf6a7c9dd06a3f47c9510c5dafd3899168c1c6e
DejaLFlynn <dejaflynn@pursuit.org>
2020-02-22 21:17:46 -0500

    adding toast container

--- lifecycle-lab/src/App.js
+++ lifecycle-lab/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
 import ToDo from './Component/Todo';
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-function App() {
-  return (
-    <div className="App">
-      Life cycle
-      <ToDo/>
+class App extends React.Component {
+  state = {
+    text: ''
+  }
+
+  render() {
+    return (
+
+      <div className="App">
+        <ToDo />
+        {/* <ToastBox/> */}
+        life cycle lab
     </div>
-  );
+    )
+  }
 }
 
 export default App;
--- lifecycle-lab/src/Component/Todo.js
+++ lifecycle-lab/src/Component/Todo.js
@@ -1,33 +1,35 @@
 import React from 'react';
-
-class ToDo extends React.Component{
-    constructor(props){
+import {toast, ToastContainer} from 'react-toastify';
+class ToDo extends React.Component {
+    constructor(props) {
         super(props)
-        this.state = {text: " hello"}
+        this.state = { text: " hello" }
     }
-    handleSubmit(event){
+    handleSubmit(event) {
         event.preventDefault()
+        let {text} = this.state
     }
-    componentDidMount(){
-    console.log("componentWillMount()")
+    componentDidMount() {
+        console.log("componentWillMount()")
     }
 
-    handleChange(event){
-        console.log(event)
-        // this.setState({text:event.target.value})
+    handleChange(event) {
+
+        this.setState({ text: event.target.value })
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
 
     }
- render(){
-     return(
-         <form onSubmit ={this.handleSubmit}>
-             <input type="text" onChange={this.handleChange}placeholder="Enter a To Do" ></input>
-            <h3></h3>
-
-         </form>
-     )
- }   
+    render() {
+       
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" onChange={this.handleChange} placeholder="Enter a To Do" ></input>
+                <h3></h3>
+            <ToastContainer/>
+            </form>
+        )
+    }
 }
 
 export default ToDo;
